68029368a6e496ed75a1bcada398e85fc929ebc3
Susan <susan.huang@tenfoldtech.com>
2020-06-23 21:08:36 +0800

    [切版] 加入單獨刪除按鈕

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ function clearTaskList (isDone) {
 // 更新 任務列表
 function updateTaskList () {
   removeAllTask();
-  taskUndone.forEach(function(element) {
-    document.querySelector('#taskUndone').innerHTML += taskTemplate(false, element)
+  taskUndone.forEach(function(element, index) {
+    document.querySelector('#taskUndone').innerHTML += taskTemplate(false, element, index)
   })
-  taskDone.forEach(function(element) {
-    document.querySelector('#taskDone').innerHTML += taskTemplate(true, element)
+  taskDone.forEach(function(element, index) {
+    document.querySelector('#taskDone').innerHTML += taskTemplate(true, element, index)
   })
   updateTaskCount();
   checkedTask();
+  deleteTask();
 }
 // 更新 任務列表數量總計
 function updateTaskCount () {
@@ -37,9 +38,9 @@ function removeAllTask() {
   document.querySelector('#taskDone').innerHTML = '';
 }
 // 模板 (渲染使用)
-function taskTemplate (isDone, text) {
+function taskTemplate (isDone, text, index) {
   let taskTemplate = `
-  <li>
+  <li data-index="${index}" class="${ isDone ?'done': 'undone'}">
     <label class="checkbox-group ${ isDone ?'isChecked': ''}">
       <input type="checkbox" class="checkbox" ${ isDone ?'checked': ''}>
       <span class="todo-list-text">${text}</span>
@@ -52,17 +53,29 @@ function taskTemplate (isDone, text) {
 // 勾選事件
 function checkedTask () {
   let nodecheckbox = document.querySelectorAll('.checkbox')
-  Array.prototype.forEach.call(nodecheckbox, function(item, index) {
+  Array.prototype.forEach.call(nodecheckbox, function(item) {
     item.addEventListener('change', (event) => {
+      var index = event.target.closest('li').dataset.index;
       if (event.target.checked) {
         taskDone.push(taskUndone[index]);
         taskUndone.splice(index, 1)
       }
       if(!event.target.checked) {
-        index = index - taskUndone.length;
         taskUndone.push(taskDone[index]);
         taskDone.splice(index, 1)
       }
+      updateTaskList();
+    })
+  })
+}
+// 刪除事件
+function deleteTask () {
+  let nodeDeleteButton = document.querySelectorAll('.todo-delete')
+  Array.prototype.forEach.call(nodeDeleteButton, function(item, index) {
+    item.addEventListener('click', function() {
+      var isUnDone = this.closest('li').className.indexOf('undone') > -1;
+      var index = this.closest('li').dataset.index;
+      isUnDone ? taskUndone.splice(index, 1) : taskDone.splice(index, 1);
       updateTaskList();
     })
   })
@@ -72,7 +85,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelector('#addTodo').addEventListener('click', function() {
     addTask(false)
   });
-  
   let nodeClearbuttons = document.querySelectorAll('.clearTaskList')
   Array.prototype.forEach.call(nodeClearbuttons, function(item) {
     item.addEventListener('click', function() {
@@ -82,5 +94,4 @@ document.addEventListener("DOMContentLoaded", function() {
       clearTaskList(item.dataset.isDone.bool())
     });
   });
-  
 });
